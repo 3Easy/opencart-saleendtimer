09add3a7b6a117b2a0b47f58873551d3d85833fe
3Easy <hello@3easy.org>
2011-01-18 11:24:24 +1100

    Base Installation Only

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/admin/');
+define('HTTP_CATALOG', 'http://opencart/');
+define('HTTP_IMAGE', 'http://opencart/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/admin/');
+define('HTTPS_IMAGE', 'http://opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/3Easy/Sites/opencart/admin/');
+define('DIR_SYSTEM', '/Users/3Easy/Sites/opencart/system/');
+define('DIR_DATABASE', '/Users/3Easy/Sites/opencart/system/database/');
+define('DIR_LANGUAGE', '/Users/3Easy/Sites/opencart/admin/language/');
+define('DIR_TEMPLATE', '/Users/3Easy/Sites/opencart/admin/view/template/');
+define('DIR_CONFIG', '/Users/3Easy/Sites/opencart/system/config/');
+define('DIR_IMAGE', '/Users/3Easy/Sites/opencart/image/');
+define('DIR_CACHE', '/Users/3Easy/Sites/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/Users/3Easy/Sites/opencart/download/');
+define('DIR_LOGS', '/Users/3Easy/Sites/opencart/system/logs/');
+define('DIR_CATALOG', '/Users/3Easy/Sites/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'local_opencart');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// DIR
+define('DIR_APPLICATION', '/Users/3Easy/Sites/opencart/catalog/');
+define('DIR_SYSTEM', '/Users/3Easy/Sites/opencart/system/');
+define('DIR_DATABASE', '/Users/3Easy/Sites/opencart/system/database/');
+define('DIR_LANGUAGE', '/Users/3Easy/Sites/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/Users/3Easy/Sites/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/3Easy/Sites/opencart/system/config/');
+define('DIR_IMAGE', '/Users/3Easy/Sites/opencart/image/');
+define('DIR_CACHE', '/Users/3Easy/Sites/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/Users/3Easy/Sites/opencart/download/');
+define('DIR_LOGS', '/Users/3Easy/Sites/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'local_opencart');
+define('DB_PREFIX', '');
+?>
